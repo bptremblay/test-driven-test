eaa67a9bda25fe4a47bcfb48d9aedae0fb746a27
Ben Tremblay <btremblay@me.com>
2015-01-04 21:03:40 -0500

    added implementation of delay TODO: testing!!!

--- js/delay.js
+++ js/delay.js
@@ -3,10 +3,30 @@
 
 // NOTE: in the spec, delay() is global and not pulled in via require()
 // So we are making it an old-school global method, which must be pulled in via a script tag.
-function delay(){
-	
+
+/**
+ *
+ * @param {Number} milliseconds
+ * @param {Object} scope
+ * @param {String} methodName
+ * @param {Array} args
+ */
+function delay(milliseconds, scope, methodName, args) {
+	var thePromise = new Promise(function(resolve, reject) {
+		window.setTimeout(function(){
+			try{
+				var method = scope[methodName];
+				var result = method.apply(scope, args);
+				resolve(result);
+			}
+			catch(error){
+				error.scope = scope;
+				error.methodName = methodName;
+				error.args = args;
+				reject(error);
+			}
+		}, milliseconds);
+	});
+	return thePromise;
 }
 
-function executor(resolve, reject){
-	
-}
